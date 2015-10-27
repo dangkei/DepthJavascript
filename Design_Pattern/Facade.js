838c394c927f44fb9c590a12e8b7a89394117b17
= <=>
2015-10-27 21:41:02 +0800

    Initial commit 20151027AM

--- Design_Pattern/Facade.js
+++ Design_Pattern/Facade.js
@@ -17,10 +17,9 @@
 var addMyEvent = function (el, ev, fn) {
     if (el.addEventListener) {
         el.addEventListener(ev, fn, false);
-    } else {
+    } else if (el.attachEvent) {
         el.attachEvent('on' + ev, fn);
-    }
-    else
+    } else
     {
         el['on' + ev] = fn;
     }
